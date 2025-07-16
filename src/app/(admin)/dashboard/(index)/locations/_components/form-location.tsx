33fedb638339f54a795961ec763961f5c48ea397
ainul <ainulmufidh@gmail.com>
2025-07-16 20:01:24 +0700

    revisi form-location

--- src/app/(admin)/dashboard/(index)/locations/_components/form-location.tsx
+++ src/app/(admin)/dashboard/(index)/locations/_components/form-location.tsx
@@ -5,116 +5,116 @@ import Link from "next/link";
 import { AlertCircle, ChevronLeft } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import {
-	Card,
-	CardContent,
-	CardDescription,
-	CardHeader,
-	CardTitle,
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { ActionResult } from "@/types";
 import { useFormState, useFormStatus } from "react-dom";
-import { postCategory, updateCategory } from "../lib/actions";
+import { postLocation, updateLocation } from "../lib/actions";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Category } from "@prisma/client";
 
 const initialState: ActionResult = {
-	error: "",
+  error: "",
 };
 
 interface FormCategoryProps {
-	type?: "ADD" | "EDIT";
-	data?: Category | null;
+  type?: "ADD" | "EDIT";
+  data?: Category | null;
 }
 
 function SubmitButton() {
-	const { pending } = useFormStatus();
+  const { pending } = useFormStatus();
 
-	return (
-		<Button type="submit" size="sm" disabled={pending}>
-			{pending ? "Loading..." : "Save Location"}
-		</Button>
-	);
+  return (
+    <Button type="submit" size="sm" disabled={pending}>
+      {pending ? "Loading..." : "Save Location"}
+    </Button>
+  );
 }
 
-export default function FormCategory({
-	data = null,
-	type = "ADD",
+export default function FormLocation({
+  data = null,
+  type = "ADD",
 }: FormCategoryProps) {
-	const updateCategoryWithId = (_: unknown, formData: FormData) =>
-		updateCategory(_, formData, data?.id);
+  const updateCategoryWithId = (_: unknown, formData: FormData) =>
+    updateLocation(_, formData, data?.id);
 
-	const [state, formAction] = useFormState(
-		type === "ADD" ? postCategory : updateCategoryWithId,
-		initialState
-	);
+  const [state, formAction] = useFormState(
+    type === "ADD" ? postLocation : updateCategoryWithId,
+    initialState
+  );
 
-	return (
-		<form action={formAction}>
-			<div className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
-				<div className="mx-auto grid max-w-[59rem] flex-1 auto-rows-max gap-4">
-					<div className="flex items-center gap-4">
-						<Button
-							variant="outline"
-							size="icon"
-							className="h-7 w-7"
-							asChild
-						>
-							<Link href="/dashboard/locations">
-								<ChevronLeft className="h-4 w-4" />
-								<span className="sr-only">Back</span>
-							</Link>
-						</Button>
-						<h1 className="flex-1 shrink-0 whitespace-nowrap text-xl font-semibold tracking-tight sm:grow-0">
-							Location Controller
-						</h1>
-						<div className="hidden items-center gap-2 md:ml-auto md:flex">
-							<Button variant="outline" size="sm">
-								Discard
-							</Button>
-							<SubmitButton />
-						</div>
-					</div>
-					<div className="grid gap-4 md:grid-cols-[1fr_250px] lg:grid-cols-3 lg:gap-8">
-						<div className="grid auto-rows-max items-start gap-4 lg:col-span-2 lg:gap-8">
-							<Card
-								x-chunk="dashboard-07-chunk-0"
-								className="w-[500px]"
-							>
-								<CardHeader>
-									<CardTitle>Location Details</CardTitle>
-									<CardDescription>
-										Lipsum dolor sit amet, consectetur
-										adipiscing elit
-									</CardDescription>
-								</CardHeader>
-								<CardContent>
-									{state.error !== "" && (
-										<Alert
-											variant="destructive"
-											className="mb-4"
-										>
-											<AlertCircle className="h-4 w-4" />
-											<AlertTitle>Error</AlertTitle>
-											<AlertDescription>
-												{state.error}
-											</AlertDescription>
-										</Alert>
-									)}
+  return (
+    <form action={formAction}>
+      <div className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
+        <div className="mx-auto grid max-w-[59rem] flex-1 auto-rows-max gap-4">
+          <div className="flex items-center gap-4">
+            <Button
+              variant="outline"
+              size="icon"
+              className="h-7 w-7"
+              asChild
+            >
+              <Link href="/dashboard/locations">
+                <ChevronLeft className="h-4 w-4" />
+                <span className="sr-only">Back</span>
+              </Link>
+            </Button>
+            <h1 className="flex-1 shrink-0 whitespace-nowrap text-xl font-semibold tracking-tight sm:grow-0">
+              Location Controller
+            </h1>
+            <div className="hidden items-center gap-2 md:ml-auto md:flex">
+              <Button variant="outline" size="sm">
+                Discard
+              </Button>
+              <SubmitButton />
+            </div>
+          </div>
+          <div className="grid gap-4 md:grid-cols-[1fr_250px] lg:grid-cols-3 lg:gap-8">
+            <div className="grid auto-rows-max items-start gap-4 lg:col-span-2 lg:gap-8">
+              <Card
+                x-chunk="dashboard-07-chunk-0"
+                className="w-[500px]"
+              >
+                <CardHeader>
+                  <CardTitle>Location Details</CardTitle>
+                  <CardDescription>
+                    Lipsum dolor sit amet, consectetur
+                    adipiscing elit
+                  </CardDescription>
+                </CardHeader>
+                <CardContent>
+                  {state.error !== "" && (
+                    <Alert
+                      variant="destructive"
+                      className="mb-4"
+                    >
+                      <AlertCircle className="h-4 w-4" />
+                      <AlertTitle>Error</AlertTitle>
+                      <AlertDescription>
+                        {state.error}
+                      </AlertDescription>
+                    </Alert>
+                  )}
 
-									<div className="grid gap-6">
-										<div className="grid gap-3">
-											<Label htmlFor="name">Name</Label>
-											<Input
-												id="name"
-												type="text"
-												name="name"
-												className="w-full"
-												defaultValue={data?.name}
-											/>
-										</div>
-										{/* <div className="grid gap-3">
+                  <div className="grid gap-6">
+                    <div className="grid gap-3">
+                      <Label htmlFor="name">Name</Label>
+                      <Input
+                        id="name"
+                        type="text"
+                        name="name"
+                        className="w-full"
+                        defaultValue={data?.name}
+                      />
+                    </div>
+                    {/* <div className="grid gap-3">
                         <Label htmlFor="description">Description</Label>
                         <Textarea
                           id="description"
@@ -122,10 +122,10 @@ export default function FormCategory({
                           className="min-h-32"
                         />
                       </div> */}
-									</div>
-								</CardContent>
-							</Card>
-							{/* <Card x-chunk="dashboard-07-chunk-1">
+                  </div>
+                </CardContent>
+              </Card>
+              {/* <Card x-chunk="dashboard-07-chunk-1">
                   <CardHeader>
                     <CardTitle>Stock</CardTitle>
                     <CardDescription>
@@ -234,7 +234,7 @@ export default function FormCategory({
                     </Button>
                   </CardFooter>
                 </Card> */}
-							{/* <Card x-chunk="dashboard-07-chunk-2">
+              {/* <Card x-chunk="dashboard-07-chunk-2">
                   <CardHeader>
                     <CardTitle>Product Category</CardTitle>
                   </CardHeader>
@@ -283,8 +283,8 @@ export default function FormCategory({
                     </div>
                   </CardContent>
                 </Card> */}
-						</div>
-						{/* <div className="grid auto-rows-max items-start gap-4 lg:gap-8">
+            </div>
+            {/* <div className="grid auto-rows-max items-start gap-4 lg:gap-8">
                 <Card x-chunk="dashboard-07-chunk-3">
                   <CardHeader>
                     <CardTitle>Product Status</CardTitle>
@@ -367,15 +367,15 @@ export default function FormCategory({
                   </CardContent>
                 </Card>
               </div> */}
-					</div>
-					<div className="flex items-center justify-center gap-2 md:hidden">
-						<Button variant="outline" size="sm">
-							Discard
-						</Button>
-						<Button size="sm">Save Product</Button>
-					</div>
-				</div>
-			</div>
-		</form>
-	);
+          </div>
+          <div className="flex items-center justify-center gap-2 md:hidden">
+            <Button variant="outline" size="sm">
+              Discard
+            </Button>
+            <Button size="sm">Save Product</Button>
+          </div>
+        </div>
+      </div>
+    </form>
+  );
 }
